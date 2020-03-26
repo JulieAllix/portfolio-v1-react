816f660f0f0c44bd78ce233715c7ebe21512b7b4
JulieAllix <allixjulie@gmail.com>
2020-03-26 20:28:05 +0100

    Add classnames to dropdown elts

--- src/components/Header/HeaderStyled.js
+++ src/components/Header/HeaderStyled.js
@@ -14,22 +14,46 @@ const HeaderStyled = styled.div`
     font-size: 1.7em;
     margin: auto .5em;
   }
-  .menu {
-    display: flex;
-    flex-direction: row;
-    font-family: ${theme.contentFont};
-    margin: auto 1em;
-    position: relative;
-  }
-  .menu-link {
-    margin: auto .3em;
-  }
-  .dropdown {
-    position: absolute;
-    top: 1.7em;
-    left: .2em;
-  }
+
   @media only screen and (min-width: 650px) {
+    .menu {
+      display: flex;
+      flex-direction: row;
+      font-family: ${theme.contentFont};
+      margin: auto 1em;
+      position: relative;
+    }
+    .menu-link {
+      margin: auto .3em;
+    }
+    .dropdown {
+      position: absolute;
+      top: 1.7em;
+      left: .2em;
+    }
+    .dropdown-item {
+    }
+    .grocereaz {
+      transform: translateX(1000px);
+      transition: .6s;
+      &.projects--open{
+        transform: translateX(0);
+      }
+    }
+    .break-free {
+      transform: translateX(1000px);
+      transition: .7s;
+      &.projects--open{
+        transform: translateX(0);
+      }
+    }
+    .study-cards {
+      transform: translateX(1000px);
+      transition: .8s;
+      &.projects--open{
+        transform: translateX(0);
+      }
+    }
 
   }
 `;
--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,39 +1,65 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import classNames from 'classnames';
 
 import HeaderStyled from './HeaderStyled';
 
-const Header = () => (
-  <HeaderStyled>
-    <div className="julie">Julie Allix</div>
-    <ul className="menu">
-      <li className="menu-link">Projects</li>
-      <ul className="dropdown">
-        <Link to="/grocereaz">
-          <li className="dropdown-item">Grocer'eaz</li>
+const Header = () => {
+  const projectIsOpen = true;
+  return (
+    <HeaderStyled>
+      <div className="julie">Julie Allix</div>
+      <ul className="menu">
+        <li className="menu-link">Projects</li>
+        <ul className="dropdown">
+          <Link to="/grocereaz">
+            <li className={classNames({
+              'projects--open': projectIsOpen,
+              'dropdown-item': true,
+              grocereaz: true,
+            })}
+            >
+              Grocer'eaz
+            </li>
+          </Link>
+          <Link to="/break-free">
+            <li className={classNames({
+              'projects--open': projectIsOpen,
+              'dropdown-item': true,
+              'break-free': true,
+            })}
+            >
+              Break Free
+            </li>
+          </Link>
+          <Link to="/study-cards">
+            <li className={classNames({
+              'projects--open': projectIsOpen,
+              'dropdown-item': true,
+              'study-cards': true,
+            })}
+            >
+              Study Cards
+            </li>
+          </Link>
+        </ul>
+        <Link to="/">
+          <li className="menu-link">Home</li>
         </Link>
-        <Link to="/break-free">
-          <li className="dropdown-item">Break Free</li>
+        <Link to="/about">
+          <li className="menu-link">About</li>
         </Link>
-        <Link to="/study-cards">
-          <li className="dropdown-item">Study Cards</li>
+        <Link to="/oclock">
+          <li className="menu-link">My training</li>
+        </Link>
+        <Link to="/contact">
+          <li className="menu-link">Contact</li>
         </Link>
       </ul>
-      <Link to="/">
-        <li className="menu-link">Home</li>
-      </Link>
-      <Link to="/about">
-        <li className="menu-link">About</li>
-      </Link>
-      <Link to="/oclock">
-        <li className="menu-link">My training</li>
-      </Link>
-      <Link to="/contact">
-        <li className="menu-link">Contact</li>
-      </Link>
-    </ul>
-  </HeaderStyled>
-);
+    </HeaderStyled>
+  );
+};
+
 
 // == Export
 export default Header;
