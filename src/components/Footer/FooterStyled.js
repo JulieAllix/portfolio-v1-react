eb1659544502c8a959716a52da40994eb0fa6eb2
JulieAllix <allixjulie@gmail.com>
2020-03-28 13:12:25 +0100

    Finish footer

--- src/components/Footer/FooterStyled.js
+++ src/components/Footer/FooterStyled.js
@@ -4,9 +4,19 @@ import styled from 'styled-components';
 const FooterStyled = styled.div`
   display: flex;
   flex-direction: row;
+  justify-content: center;
+  border: 1px solid lightgrey;
+  .footer-icon {
+    margin: .5em 1.5em;
+  }
 
   @media only screen and (min-width: 650px) {
-
+    .contact {
+      display: none;
+    }
+    .footer-icon {
+      margin: .5em 2em;
+    }
   }
 `;
 
--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -5,7 +5,18 @@ import FooterStyled from './FooterStyled';
 
 const Footer = () => (
   <FooterStyled>
-    Footer
+    <div className="github footer-icon">
+      <Icon name="github" size="big" color="grey" />
+    </div>
+    <div className="contact footer-icon">
+      <Icon name="mail" size="big" color="grey" />
+    </div>
+    <div className="linkedIn footer-icon">
+      <Icon name="linkedin" size="big" color="grey" />
+    </div>
+    <div className="wordPress footer-icon">
+      <Icon name="wordpress" size="big" color="grey" />
+    </div>
   </FooterStyled>
 );
 
