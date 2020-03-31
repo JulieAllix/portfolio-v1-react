87cbdc889a2a9cb4ab364a713973ab35e981c5f7
JulieAllix <allixjulie@gmail.com>
2020-03-31 12:58:17 +0200

    Change home component into class

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,16 +1,31 @@
-import React from 'react';
+/*
+
+// import React, { useRef } from 'react';
 // import PropTypes from 'prop-types';
 // import classNames from 'classnames';
 
 import HomeStyled from './HomeStyled';
 
 const Home = () => {
+  const grocereaz = useRef(null);
   const handleClick = () => {
     console.log('clic');
   };
+  /*
   const chbg = (color) => {
-    document.getElementById("grocereaz").style.backgroundColor = color;
+    //grocereaz.style.backgroundColor = color;
+    console.log(grocereaz);
+    console.log('coucou');
+    //grocereaz.style.background = color;
   };
+  
+  const test = () => {
+    console.log('test');
+  };
+  const testOut = () => {
+    console.log('test out');
+  };
+  test();
   return (
     <HomeStyled>
       <section className="projects-section">
@@ -22,7 +37,13 @@ const Home = () => {
       </section>
       <section className="skills-section">
         <div className="skills-wrapper">
-          <span className="react" onMouseOver={chbg('yellow')} onMouseOut={chbg('white')}>React</span>
+          <span
+            id="react"
+            onMouseOver={test()}
+            onMouseOut={testOut()}
+          >
+            React
+          </span>
         </div>
       </section>
     </HomeStyled>
@@ -33,6 +54,54 @@ Home.propTypes = {
   menuIsOpen: PropTypes.bool.isRequired,
   changeOpeningStatus: PropTypes.func.isRequired,
 };
-*/
+
 // == Export
+export default Home;
+*/
+import React from 'react';
+import HomeStyled from './HomeStyled';
+
+// eslint-disable-next-line react/prefer-stateless-function
+class Home extends React.Component {
+  constructor() {
+    super();
+    console.log('constructor : instanciation');
+    this.test = this.test.bind(this);
+    this.testOut = this.testOut.bind(this);
+  }
+
+  test() {
+    console.log('test');
+  }
+
+  testOut() {
+    console.log('test out');
+  }
+
+  render() {
+    return (
+      <HomeStyled>
+        <section className="projects-section">
+          <div className="projects-wrapper">
+            <div className="grocereaz">
+              Grocer'eaz
+            </div>
+          </div>
+        </section>
+        <section className="skills-section">
+          <div className="skills-wrapper">
+            <span
+              id="react"
+              onMouseOver={this.test}
+              onMouseOut={this.testOut}
+            >
+              React
+            </span>
+          </div>
+        </section>
+      </HomeStyled>
+    );
+  }
+}
+
 export default Home;
