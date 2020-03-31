24900957c497c30fe15f5106cc2fe0de778dc1ee
JulieAllix <allixjulie@gmail.com>
2020-03-31 14:27:41 +0200

    Add all projects

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,5 +1,4 @@
 import React from 'react';
-// import PropTypes from 'prop-types';
 import classNames from 'classnames';
 import HomeStyled from './HomeStyled';
 
@@ -7,11 +6,13 @@ import HomeStyled from './HomeStyled';
 class Home extends React.Component {
   constructor(props) {
     super(props);
-    // this.grocereaz = React.createRef();
     this.test = this.test.bind(this);
     this.testOut = this.testOut.bind(this);
     this.state = {
       grocereazIsHighlighted: false,
+      portfolioIsHighlighted: false,
+      breakFreeIsHighlighted: false,
+      studyCardsIsHighlighted: false,
     };
   }
 
@@ -33,14 +34,36 @@ class Home extends React.Component {
         <section className="projects-section">
           <div className="projects-wrapper">
             <div
-              id="grocereaz"
-              ref={this.grocereaz}
               className={classNames({
                 grocereaz: true,
                 'highlighted--project': this.state.grocereazIsHighlighted,
               })}
             >
               Grocer'eaz
+            </div>
+            <div
+              className={classNames({
+                portfolio: true,
+                'highlighted--project': this.state.portfolioIsHighlighted,
+              })}
+            >
+              This portfolio
+            </div>
+            <div
+              className={classNames({
+                breakFree: true,
+                'highlighted--project': this.state.breakFreeIsHighlighted,
+              })}
+            >
+              Break Free
+            </div>
+            <div
+              className={classNames({
+                studyCards: true,
+                'highlighted--project': this.state.studyCardsIsHighlighted,
+              })}
+            >
+              Study Cards
             </div>
           </div>
         </section>
@@ -59,9 +82,5 @@ class Home extends React.Component {
     );
   }
 }
-/*
-Home.propTypes = {
-  grocereazIsHighlighted: PropTypes.bool.isRequired,
-};
-*/
+
 export default Home;
