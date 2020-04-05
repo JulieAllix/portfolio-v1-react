cad5d07558c942f35ba325c9304c660753c07d55
JulieAllix <allixjulie@gmail.com>
2020-04-05 15:43:39 +0200

    Commit before semantic-ui test

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import React from 'react';
 import { render } from 'react-dom';
 import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
+import 'semantic-ui-css/semantic.min.css';
 
 // == Import : local
 import App from 'src/components/App';
