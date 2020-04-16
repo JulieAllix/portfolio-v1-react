468e8d6c04bb89a7846bdbee5580affc7de60a05
JulieAllix <allixjulie@gmail.com>
2020-04-16 18:27:12 +0200

    Attempts at making tests

--- tests/store/headerReducer.test.js
+++ tests/store/headerReducer.test.js
@@ -9,14 +9,3 @@ describe('Tests on the header reducer', () => {
     });
   });
 });
-/*
-var assert = require('assert');
-
-describe('Array', function() {
-  describe('#indexOf()', function() {
-    it('should return -1 when the value is not present', function() {
-      assert.equal([1, 2, 3].indexOf(4), -1);
-    });
-  });
-});
-*/
